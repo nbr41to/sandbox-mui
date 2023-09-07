93eeda6205d295772b1877eae01b783efc916673
Knob <59198478+nbr41to@users.noreply.github.com>
2023-09-07 21:26:15 +0900

    Create layout.tsx

---
+++ src/pages/layout.tsx
@@ -0,0 +1,14 @@
+import { Button, Stack, Container, Box, Grid } from '@mui/material';
+
+export default function Layout() {
+  return (
+    <div>
+      <h1>Layout</h1>
+      <Button>Button</Button>
+      <Container></Container>
+      <Box></Box>
+      <Stack></Stack>
+      <Grid></Grid>
+    </div>
+  );
+}
