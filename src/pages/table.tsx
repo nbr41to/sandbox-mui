19d6e90dc8348f4c8b5ac70d73fca840fd31b1f6
Knob <59198478+nbr41to@users.noreply.github.com>
2023-09-07 21:26:15 +0900

    Create table.tsx

---
+++ src/pages/table.tsx
@@ -0,0 +1,10 @@
+import { Container } from '@mui/material';
+import { Table } from '@/components/Table';
+
+export default function TablePage() {
+  return (
+    <Container>
+      <Table />
+    </Container>
+  );
+}
