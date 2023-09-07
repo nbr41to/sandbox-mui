32e3630ca5ee0697136df53e64da871767d8b419
Knob <59198478+nbr41to@users.noreply.github.com>
2023-09-07 21:26:12 +0900

    Update _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,14 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import type { AppProps } from 'next/app';
+import { Header } from '@/components/Header';
+import '@/styles/globals.css';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <div className='flex divide-x'>
+      <Header />
+      <div className='p-6 flex-grow'>
+        <Component {...pageProps} />
+      </div>
+    </div>
+  );
 }
